8f3b17297477c69afd0c668abff2dadddddb1917
pdevra <94073782+pdevra@users.noreply.github.com>
2022-07-31 19:03:54 +0530

    Add files via upload

---
+++ game of functions.py
@@ -0,0 +1,12 @@
+# Game of "Functions"
+
+#  Write a Python function to sum all the numbers in a list.
+# Sample List : (8, 2, 3, 0, 7)
+# Expected Output : 20
+
+def sum(numbers):
+    total = 0
+    for x in numbers:
+        total += x
+    return total
+print(sum((8, 2, 3, 0, 7)))
